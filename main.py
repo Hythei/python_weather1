8bc17cd3776b09362153bc22c0bbc0350bad6c3d
Hythei <heikki.hyttinen16@gmail.com>
2026-05-09 23:49:20 +0300

    Progress

--- main.py
+++ main.py
@@ -16,6 +16,12 @@ params = {
     "aqi":"no"
 }
 
+# Current.json refers to current weather
 response = requests.get(f"{BASE_URL}/current.json", params=params)
 
-print(response.json())
+data = response.json()
+print(data)
+weather_current = data["current"]
+weather_location = data["location"]
+
+print(f"Location: {weather_location['name']}, current temperature is {weather_current['temp_c']} degrees Celsius")

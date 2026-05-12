b3e06ab646ad40c6e2caacb964043ca10d509c96
Hythei <heikki.hyttinen16@gmail.com>
2026-05-12 23:28:54 +0300

    MongoDB connection successful

--- main.py
+++ main.py
@@ -2,14 +2,24 @@ import os
 import requests
 from datetime import datetime
 from dotenv import load_dotenv
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
 
 load_dotenv()
 
 # MONGODB URL/API
-
+MONGODB_URL = os.getenv("MONGODB_URL")
+client = MongoClient(MONGODB_URL, server_api=ServerApi('1'))
 # Weather API Key
 API_KEY = os.getenv("API_KEY")
 BASE_URL = "https://api.weatherapi.com/v1"
+
+# This thing checks that we have a successful MongoDB connection
+try:
+    client.admin.command('ping')
+    print("Connection to MongoDB successful")
+except Exception as e:
+    print(f"Connection to MongoDB failed: {e}")
 
 location = input("Enter a location: ")
 
@@ -28,7 +38,7 @@ weather_current = data["current"]
 weather_location = data["location"]
 
 # Dictionary that will be sent to MongoDB
-# match_prediction will be used to showcase
+# match_prediction will be used to showcase CRUD-functions
 weather_data_model = {
     "location": weather_location['name'],
     "temperature": weather_current['temp_c'],
@@ -37,3 +47,4 @@ weather_data_model = {
 }
 
 print(f"Location: {weather_location['name']}, current temperature is {weather_current['temp_c']} degrees Celsius")
+print(f"Date: {datetime.now()}")

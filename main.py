27c74e6ceba033982b1579d8c48556ad80a2137c
Hythei <heikki.hyttinen16@gmail.com>
2026-07-27 19:15:32 +0300

    GET progress

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from typing import Optional, List
 
+import pymongo
 from fastapi import FastAPI, Body, HTTPException, status
 from fastapi.responses import Response
 from pydantic import ConfigDict, BaseModel, Field, EmailStr
@@ -21,13 +22,30 @@ app = FastAPI(
     title="Weather Information API",
     summary="A simple application to fetch information from Weather Information API",
 )
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+client = AsyncMongoClient(os.getenv("MONGODB_URL"), server_api=pymongo.server_api.ServerApi(version="1", strict=True,deprecation_errors=True))
+db = client.weather_test1
+weather_collection = db.get_collection("weather_information")
+
+PyObjectId = Annotated[str, BeforeValidator(str)]
 
 
-def weather_query():
-    pass
+class WeatherInformationModel(BaseModel):
+    id: Optional[PyObjectId] = Field(alias="_id", default=None)
+    location: str = Field(alias="location")
+    temperature: float = Field(alias="temperature")
+    date: str = Field(alias="date")
+    match_prediction: bool = Field(alias="match_prediction")
+    model_config = ConfigDict(from_attributes=True)
 
-def main():
-    pass
+class WeatherInformation(BaseModel):
+    weather_information: List[WeatherInformationModel]
+
+@app.get(
+    "/weather_information",
+    response_description="Get all weather information",
+    response_model=WeatherInformation,
+    response_model_by_alias=True,
+)
+
+async def get_weather_information():
+    return WeatherInformation(weather_information=await weather_collection.find({}).to_list(None))

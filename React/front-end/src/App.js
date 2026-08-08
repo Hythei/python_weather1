5b45b0371dc5bc704e8078288e8458324fa3a6c9
Hythei <heikki.hyttinen16@gmail.com>
2026-08-08 23:55:40 +0300

    MongoDB/React CRUD Progress

--- React/front-end/src/App.js
+++ React/front-end/src/App.js
@@ -23,33 +23,52 @@ const App = () => {
         setFormData({
             ...formData,
             [e.target.name]: prediction
-        })
+        });
+    };
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+
     }
 
     return (
-        <div className="container mt-4">
-            <table className="table table-striped table-bordered table-hover">
-                <thead>
-                    <tr>
-                        <th>id</th>
-                        <th>Location</th>
-                        <th>Temperature</th>
-                        <th>Match prediction</th>
-                        <th>Date</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {documents.map((document, index) => (
-                        <tr key={index}>
-                            <td>{document._id}</td>
-                            <td>{document.location}</td>
-                            <td>{document.temperature}</td>
-                            <td>{document.match_prediction ? "Yes" : "No"}</td>
-                            <td>{document.date}</td>
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">
+                        Weather CRUD
+                    </a>
+                </div>
+            </nav>
+            <div className="container">
+                <from onSubmit={handleFormSubmit}>
+
+                </from>
+            </div>
+            <div className="container mt-4">
+                <table className="table table-striped table-bordered table-hover">
+                    <thead>
+                        <tr>
+                            <th>id</th>
+                            <th>Location</th>
+                            <th>Temperature</th>
+                            <th>Match prediction</th>
+                            <th>Date</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {documents.map((document, index) => (
+                            <tr key={index}>
+                                <td>{document._id}</td>
+                                <td>{document.location}</td>
+                                <td>{document.temperature}</td>
+                                <td>{document.match_prediction ? "Yes" : "No"}</td>
+                                <td>{document.date}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 }
